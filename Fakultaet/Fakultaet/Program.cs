f6c15c9ffde0736bc6499b49e2df8ffec7d46b4f
Martin Plieske <martin.plieske@gmail.com>
2021-10-27 18:35:05 +0200

    woche5: Fakultät Teil 1

---
+++ Fakultaet/Fakultaet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fakultaet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateFactorial(5));
+        }
+
+        public static double CalculateFactorial(uint number)
+        {
+            if(number <= 1)
+            {
+                return number;
+            }
+
+            return number * CalculateFactorial(number - 1);
+        }
+    }
+}
