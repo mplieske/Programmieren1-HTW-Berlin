a0b53437fba188bbbdb3dbe4421ea2094ba6b496
Martin Plieske <martin.plieske@gmail.com>
2021-10-27 18:26:01 +0200

    woche5: Lösung für 'Anweisungen umsetzen' gefunden

--- AnweisungenUmsetzen/AnweisungenUmsetzen/Program.cs
+++ AnweisungenUmsetzen/AnweisungenUmsetzen/Program.cs
@@ -9,7 +9,33 @@ namespace AnweisungenUmsetzen
             Console.WriteLine("Bitte geben Sie Ihren Vor- und Nachnamen ein:");
             string name = Console.ReadLine();
 
-            Console.WriteLine($"Hallo {name}!");
+            string greeting = ErmittleBegruessung(DateTime.Now.Hour);
+            Console.WriteLine($"{greeting} {name}!");
+        }
+
+        public static string ErmittleBegruessung(int stunde)
+        {
+            if (stunde >= 5 && stunde < 11)
+            {
+                return "Guten Morgen";
+            }
+
+            if (stunde >= 11 && stunde < 17)
+            {
+                return "Guten Tag";
+            }
+
+            if (stunde >= 17 && stunde < 23)
+            {
+                return "Guten Abend";
+            }
+
+            if ((stunde >= 23 && stunde < 24) || (stunde >= 0 && stunde < 5))
+            {
+                return "Gute Nacht";
+            }
+
+            return "";
         }
     }
 }
