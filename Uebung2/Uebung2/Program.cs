0586b7060fe51aa665b80b5f6e2c3c202a41e355
Martin Plieske <martin.plieske@gmail.com>
2021-10-27 16:04:13 +0200

    woche2: fertig mit normaler Aufgabe

---
+++ Uebung2/Uebung2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uebung2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Request user data
+            Console.WriteLine("Bitte geben Sie Ihren Vornamen ein: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Bitte geben Sie Ihren Nachnamen ein: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Bitte geben Sie Ihre Straße ein: ");
+            string street = Console.ReadLine();
+
+            Console.WriteLine("Bitte geben Sie Ihre PLZ ein: ");
+            string postalCode = Console.ReadLine();
+
+            Console.WriteLine("Bitte geben Sie Ihre Stadt ein: ");
+            string city = Console.ReadLine();
+
+            // Print user data.
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine(street);
+            Console.WriteLine(postalCode + " " + city);
+        }
+    }
+}
