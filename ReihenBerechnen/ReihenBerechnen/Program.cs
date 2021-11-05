1fea8574e38769ba0408fa5219332a4b388d875a
Martin Plieske <martin.plieske@gmail.com>
2021-11-05 16:25:01 +0100

    woche6: kommentiert und teil 1 u 2 abgeschlossen

--- ReihenBerechnen/ReihenBerechnen/Program.cs
+++ ReihenBerechnen/ReihenBerechnen/Program.cs
@@ -6,27 +6,79 @@ namespace ReihenBerechnen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Teil 1
+            uint n1 = 10;
+            uint n2 = 25;
+
+            decimal resultN1 = BerechneSumme1MitEndwert(n1);
+            decimal resultN2 = BerechneSumme1MitEndwert(n2);
+
+            Console.WriteLine($"Result of '{nameof(n1)}' ('{n1}') is '{resultN1}'.");
+            Console.WriteLine($"Result of '{nameof(n2)}' ('{n2}') is '{resultN2}'.");
+
+            // Teil 2
+            uint n3 = 100;
+            uint n4 = 1000;
+
+            decimal resultN3 = BerechneSumme2MitEndwert(n3);
+            decimal resultN4 = BerechneSumme2MitEndwert(n4);
+
+            Console.WriteLine($"Result of '{nameof(n3)}' ('{n3}') is '{resultN3}'.");
+            Console.WriteLine($"Result of '{nameof(n4)}' ('{n4}') is '{resultN4}'.");
         }
 
+        // Rekursive Fakultät Methode
         public static decimal FakultaetVon(uint zahl)
         {
+            // 0! oder 1! sind jeweils 1 (Abbruchbedingung für Rekursion)
             if (zahl <= 1)
             {
-                return zahl;
+                return 1;
             }
 
-            return FakultaetVon(zahl - 1);
+            // Da n! = n * (n-1)! -> zahl ist hier n
+            return zahl * FakultaetVon(zahl - 1);
         }
 
+        // Kernmethode Teil 1
         public static decimal BerechneSumme1MitEndwert(uint endwert)
         {
-            decimal number = 1;
-            for (uint i = 1; i <= endwert; i++)
+            // initialisieren der Summe mit 0
+            decimal sum = 0;
+
+            for (uint i = 0; i <= endwert; i++)
             {
-                number += 1 / FakultaetVon(i);
+                // Berechnen der Fakultät im Nenner
+                decimal iFakultaet = FakultaetVon(i);
+
+                // Berechnen der Iteration und auf Summe aufaddieren
+                sum += 1 / iFakultaet;
             }
-            return number;
+
+            // Rückgabe der Summe
+            return sum;
+        }
+
+        // Kernmethode Teil 2
+        public static decimal BerechneSumme2MitEndwert(uint endwert)
+        {
+            // Initialisieren der Summe mit 0
+            decimal sum = 0;
+
+            for (uint i = 0; i <= endwert; i++)
+            {
+                // Berechnung der Potenz
+                double powerOfI = Math.Pow(-1, i);
+
+                // Konvertieren zum Zieltyp Decimal
+                decimal powerOfIAsDecimal = Convert.ToDecimal(powerOfI);
+
+                // Berechnung
+                sum += powerOfIAsDecimal / (2m * i + 1m);
+            }
+
+            // Rückgabe der Summe mal 4
+            return sum * 4;
         }
     }
 }
