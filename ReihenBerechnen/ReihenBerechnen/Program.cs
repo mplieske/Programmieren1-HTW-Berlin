b4857c00b517482d0605f1fcd6dc2abec629bd85
Martin Plieske <martin.plieske@gmail.com>
2021-11-05 14:11:58 +0100

    woche6: created project ReihenBerechnen

---
+++ ReihenBerechnen/ReihenBerechnen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReihenBerechnen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
