f66d2b6877ab2b7cd3bc0651de4cf2694ea3356c
Martin Plieske <martin.plieske@gmail.com>
2021-11-19 00:30:04 +0100

    woche8: normale aufgabe Teil 1 + 2

---
+++ DatumBestimmen/DatumBestimmen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DatumBestimmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] ErmittleTagMonatJahr(string datum)
+        {
+            if (!IstGueltigesDatum(datum))
+            {
+                return null;
+            }
+
+            string[] dateParts = datum.Split(".");
+            int[] dateIntArray = new int[3];
+            for (int i = 0; i < dateParts.Length; i++)
+            {
+                dateIntArray[i] = int.Parse(dateParts[i]);
+            }
+
+            return dateIntArray;
+        }
+
+        public static bool IstGueltigesDatum(string datum)
+        {
+            return DateTime.TryParseExact(datum, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out _);
+        }
+    }
+}
