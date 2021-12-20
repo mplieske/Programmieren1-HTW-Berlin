2b5a845274a28a8314224893559db557ba76cff3
Martin Plieske <martin.plieske@gmail.com>
2021-12-20 21:48:39 +0100

    woche11: finished with intermediate exercise

---
+++ KlassenErstellenTeil2/KlassenErstellenTeil2/Linie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlassenErstellenTeil2
+{
+    class Linie
+    {
+        public Punkt StartPunkt { get; set; }
+        public Punkt EndPunkt { get; set; }
+
+        public double Laenge
+        {
+            get { return StartPunkt.AbstandZu(EndPunkt); }
+        }
+
+        public Linie(Punkt startPunkt, Punkt endPunkt)
+        {
+            StartPunkt = startPunkt;
+            EndPunkt = endPunkt;
+        }
+
+        /// <summary>
+        /// Moves a Linie by a Vektor.
+        /// </summary>
+        /// <param name="verschiebeVektor">Vektor to move the Linie by.</param>
+        /// <returns>Returns a new Linie which is this Linie moved by the Vektor verschiebeVektor.</returns>
+        public Linie VerschiebeUmVektor(Vektor verschiebeVektor)
+        {
+            Punkt startPunkt = StartPunkt.AlsVektor().Addiere(verschiebeVektor).AlsPunkt();
+            Punkt endPunkt = EndPunkt.AlsVektor().Addiere(verschiebeVektor).AlsPunkt();
+            return new Linie(startPunkt, endPunkt);
+        }
+
+        /// <summary>
+        /// Generates a JSON string representation of this Linie.
+        /// </summary>
+        /// <returns>Returns the generated JSON string.</returns>
+        public override string ToString()
+        {
+            return $"{{StartPunkt: {StartPunkt}, EndPunkt: {EndPunkt}, Laenge: '{Laenge}'}}";
+        }
+    }
+}
---
+++ KlassenErstellenTeil2/KlassenErstellenTeil2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KlassenErstellenTeil2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt punkt1 = new Punkt(1, 2, 3);
+            Punkt punkt2 = new Punkt(3, 2, 1);
+            Vektor vektor1 = Punkt.BildeVektor(punkt1, punkt2);
+            Vektor vektor2 = punkt1.AlsVektor();
+            Console.WriteLine($"punkt1: '{punkt1}'");
+            Console.WriteLine($"punkt2: '{punkt2}'");
+            Console.WriteLine($"Abstand zwischen punkt1 und punkt2: '{Punkt.AbstandZwischen(punkt1, punkt2)}'");
+            Console.WriteLine($"vektor1: '{vektor1}'");
+            Console.WriteLine($"vektor2: '{vektor2}'");
+
+            Vektor vektor3 = vektor1.Addiere(vektor2);
+            Console.WriteLine($"'{vektor1}' + '{vektor2}' = '{vektor3}'");
+
+            Vektor vektor4 = vektor3.Subtrahiere(vektor2);
+            Console.WriteLine($"'{vektor3}' - '{vektor2}' = '{vektor4}'");
+
+            Vektor vektor5 = vektor2.MultipliziereSkalar(2);
+            Console.WriteLine($"'{vektor2}'*2 = '{vektor5}'");
+
+            Vektor vektor6 = punkt1.AlsVektor().BildeKreuzprodukt(punkt2.AlsVektor());
+            Console.WriteLine($"'{punkt1.AlsVektor()}' x '{punkt2.AlsVektor()}' = '{vektor6}'");
+
+            double skalarprodukt = Vektor.BerechneSkalarprodukt(punkt1.AlsVektor(), punkt2.AlsVektor());
+            Console.WriteLine($"'{punkt1.AlsVektor()}' * '{punkt2.AlsVektor()}' = '{skalarprodukt}'");
+
+            Linie linie1 = new Linie(punkt1, punkt2);
+            Console.WriteLine(linie1);
+
+            Linie linie2 = linie1.VerschiebeUmVektor(new Vektor(1, 1, 1));
+            Console.WriteLine(linie2);
+        }
+    }
+}
