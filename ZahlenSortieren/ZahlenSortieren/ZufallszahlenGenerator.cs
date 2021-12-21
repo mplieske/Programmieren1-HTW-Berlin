ae6b6b0f9e17a49e133609d7ca3373e8abf80ac5
Martin Plieske <martin.plieske@gmail.com>
2021-12-22 00:50:14 +0100

    woche11: finished with advanced exercise

---
+++ ZahlenSortieren/ZahlenSortieren/ZufallszahlenGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZahlenSortieren
+{
+    class ZufallszahlenGenerator
+    {
+        private readonly int anzahl;
+        private readonly int untergrenze;
+        private readonly int obergrenze;
+
+        public ZufallszahlenGenerator(int anzahl, int untergrenze, int obergrenze)
+        {
+            this.anzahl = anzahl;
+            this.untergrenze = untergrenze;
+            this.obergrenze = obergrenze;
+        }
+
+        public int[] ErzeugeZufallszahlen()
+        {
+            if (anzahl <= 0 || untergrenze >= obergrenze)
+            {
+                return null;
+            }
+
+            Random random = new Random();
+
+            int[] randomNumbers = new int[anzahl];
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = random.Next(untergrenze, obergrenze);
+            }
+
+            Array.Sort(randomNumbers);
+            return randomNumbers;
+        }
+    }
+}
